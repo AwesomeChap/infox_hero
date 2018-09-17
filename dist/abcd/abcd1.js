0f3900ed551c73173f04e5526f8544f78920b08d
Jatin Kumar <jatin15011999@gmail.com>
2018-09-17 17:43:53 +0530

    boom boom baybey

--- dist/abcd/abcd1.js
+++ dist/abcd/abcd1.js
@@ -4,14 +4,18 @@
 particleFire.install( { THREE: THREE } );
 
 var cometContainer = document.getElementById('comet'),
-    cometContainer2 = document.getElementById('comet2');
+    cometContainer2 = document.getElementById('comet2'),
+    cometContainer3 = document.getElementById('comet3'),
+    cometContainer4 = document.getElementById('comet4'),
+    cometContainer5 = document.getElementById('comet5'),
+    cometContainer6 = document.getElementById('comet6');
 
 var width  = $(cometContainer).width(),
     height = $(cometContainer).height(),
     clock = new THREE.Clock(),
     scene,
     camera,
-    renderer,renderer12,
+    renderer,renderer12,renderer13,renderer14,renderer15,renderer16
     loader = new THREE.JSONLoader(),
     textureLoader = new THREE.TextureLoader();
 
@@ -24,7 +28,7 @@ camera.position.y = 5;
 // camera.lookAt(new THREE.Vector3(10,0,35));
 
  // add controls
- cameraControl = new THREE.TrackballControls( camera );
+//  cameraControl = new THREE.TrackballControls( camera );
 
 renderer = new THREE.WebGLRenderer( { antialias: true , alpha: true } );
 renderer.setSize( width, height );
@@ -42,8 +46,44 @@ renderer12.domElement.style.position = 'absolute';
 renderer12.domElement.style.top = '0px';
 renderer12.domElement.style.backgroundColor = 'transparent';
 
+renderer13 = new THREE.WebGLRenderer( { antialias: true , alpha: true } );
+renderer13.setSize( width, height );
+// renderer13.setClearColor( '#000000',1 );
+renderer13.setClearColor( '#000000',0 );
+renderer13.domElement.style.position = 'absolute';
+renderer13.domElement.style.top = '0px';
+renderer13.domElement.style.backgroundColor = 'transparent';
+
+renderer14 = new THREE.WebGLRenderer( { antialias: true , alpha: true } );
+renderer14.setSize( width, height );
+// renderer14.setClearColor( '#000000',1 );
+renderer14.setClearColor( '#000000',0 );
+renderer14.domElement.style.position = 'absolute';
+renderer14.domElement.style.top = '0px';
+renderer14.domElement.style.backgroundColor = 'transparent';
+
+renderer15 = new THREE.WebGLRenderer( { antialias: true , alpha: true } );
+renderer15.setSize( width, height );
+// renderer15.setClearColor( '#000000',1 );
+renderer15.setClearColor( '#000000',0 );
+renderer15.domElement.style.position = 'absolute';
+renderer15.domElement.style.top = '0px';
+renderer15.domElement.style.backgroundColor = 'transparent';
+
+renderer16 = new THREE.WebGLRenderer( { antialias: true , alpha: true } );
+renderer16.setSize( width, height );
+// renderer16.setClearColor( '#000000',1 );
+renderer16.setClearColor( '#000000',0 );
+renderer16.domElement.style.position = 'absolute';
+renderer16.domElement.style.top = '0px';
+renderer16.domElement.style.backgroundColor = 'transparent';
+
 cometContainer.appendChild( renderer.domElement );
 cometContainer2.appendChild( renderer12.domElement );
+cometContainer3.appendChild( renderer13.domElement );
+cometContainer4.appendChild( renderer14.domElement );
+cometContainer5.appendChild( renderer15.domElement );
+cometContainer6.appendChild( renderer16.domElement );
 // document.body.appendChild( renderer.domElement );
 
 scene.add( new THREE.DirectionalLight( '#ccc' ) );
@@ -89,8 +129,32 @@ var string2 ='<div class="model-wrapper">'+
             '<div id="model-text2"></div>'+
             '</div>';
 
+var string3 ='<div class="model-wrapper">'+
+            '<x-model id="model3" src="obj/sphere.gltf"></x-model>'+
+            '<div id="model-text3"></div>'+
+            '</div>';
+
+var string4 ='<div class="model-wrapper">'+
+            '<x-model id="model4" src="obj/sphere.gltf"></x-model>'+
+            '<div id="model-text4"></div>'+
+            '</div>';
+
+var string5 ='<div class="model-wrapper">'+
+            '<x-model id="model5" src="obj/sphere.gltf"></x-model>'+
+            '<div id="model-text5"></div>'+
+            '</div>'; 
+            
+var string6 ='<div class="model-wrapper">'+
+            '<x-model id="model6" src="obj/sphere.gltf"></x-model>'+
+            '<div id="model-text6"></div>'+
+            '</div>';
+
 var scene2 = new THREE.Scene(),
-    scene22 = new THREE.Scene();
+    scene22 = new THREE.Scene(),
+    scene23 = new THREE.Scene(),
+    scene24 = new THREE.Scene(),
+    scene25 = new THREE.Scene(),
+    scene26 = new THREE.Scene();
 
 var renderer2, renderer22;
 renderer2 = new THREE.CSS3DRenderer();
@@ -107,17 +171,61 @@ renderer22.domElement.style.top = 0;
 renderer22.domElement.style.left = 0;
 // rebderer22.domElement.style.overflow = 'hidden';
 
+renderer23 = new THREE.CSS3DRenderer();
+renderer23.setSize(width, height);
+renderer23.domElement.style.position = 'relative';
+renderer23.domElement.style.top = 0;
+renderer23.domElement.style.left = 0;
+// rebderer22.domElement.style.overflow = 'hidden';
+
+renderer24 = new THREE.CSS3DRenderer();
+renderer24.setSize(width, height);
+renderer24.domElement.style.position = 'relative';
+renderer24.domElement.style.top = 0;
+renderer24.domElement.style.left = 0;
+// rebderer22.domElement.style.overflow = 'hidden';
+
+renderer25 = new THREE.CSS3DRenderer();
+renderer25.setSize(width, height);
+renderer25.domElement.style.position = 'relative';
+renderer25.domElement.style.top = 0;
+renderer25.domElement.style.left = 0;
+// rebderer22.domElement.style.overflow = 'hidden';
+
+renderer26 = new THREE.CSS3DRenderer();
+renderer26.setSize(width, height);
+renderer26.domElement.style.position = 'relative';
+renderer26.domElement.style.top = 0;
+renderer26.domElement.style.left = 0;
+// rebderer22.domElement.style.overflow = 'hidden';
+
 cometContainer.appendChild( renderer2.domElement );
 cometContainer2.appendChild( renderer22.domElement );
+cometContainer3.appendChild( renderer23.domElement );
+cometContainer4.appendChild( renderer24.domElement );
+cometContainer5.appendChild( renderer25.domElement );
+cometContainer6.appendChild( renderer26.domElement );
 
 var clone = createCSS3DObject(string),
-    clone2 = createCSS3DObject(string2);;
+    clone2 = createCSS3DObject(string2),
+    clone3 = createCSS3DObject(string3),
+    clone4 = createCSS3DObject(string4),
+    clone5 = createCSS3DObject(string5),
+    clone6 = createCSS3DObject(string6);
 
 clone.position.set(0,3.5,0);
 clone2.position.set(0,3.5,0);
+clone3.position.set(0,3.5,0);
+clone4.position.set(0,3.5,0);
+clone5.position.set(0,3.5,0);
+clone6.position.set(0,3.5,0);
 
 scene2.add(clone);
 scene22.add(clone2);
+scene23.add(clone3);
+scene24.add(clone4);
+scene25.add(clone5);
+scene26.add(clone6);
 
 function createCSS3DObject(s) {
 	// create outerdiv and set inner HTML from supplied string
@@ -163,7 +271,7 @@ function rotateCube() {
 	particleFireMesh.material.update( delta );
 
 	// update the camera
-	cameraControl.update();
+	// cameraControl.update();
 
 	camera.lookAt( scene.position );
     renderer.render( scene, camera );
@@ -171,6 +279,18 @@ function rotateCube() {
     
     renderer12.render( scene, camera );
     renderer22.render( scene22, camera );
+
+    renderer13.render( scene, camera );
+    renderer23.render( scene23, camera );
+
+    renderer14.render( scene, camera );
+    renderer24.render( scene24, camera );
+
+    renderer15.render( scene, camera );
+    renderer25.render( scene25, camera );
+
+    renderer16.render( scene, camera );
+    renderer26.render( scene26, camera );
 
 } )();
 
@@ -242,6 +362,114 @@ $(document).ready(function(){
             });
             $("#model2").css({display:'inline-block'}).promise().done(function(){
                 $("#model2").animate({transform:'scale(1,1)'});
+        });
+    });
+
+    $("#model-text3").hover(function(){
+        $('#model-text3').html('Sample').promise().done(function(){});
+        $("#model3").css({display:'none'}).promise().done(function(){
+            $('#model-text3').animate(
+                {
+                    fontSize:'2px',
+                },500, 'easeOutBack',function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+        });
+        }, function(){
+            $('#model-text3').html('').promise().done(function(){});
+            $('#model-text3').animate(
+                {
+                    fontSize:'0.5px',
+                },0,function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+            $("#model3").css({display:'inline-block'}).promise().done(function(){
+                $("#model3").animate({transform:'scale(1,1)'});
+        });
+    });
+
+    $("#model-text4").hover(function(){
+        $('#model-text4').html('Sample').promise().done(function(){});
+        $("#model4").css({display:'none'}).promise().done(function(){
+            $('#model-text4').animate(
+                {
+                    fontSize:'2px',
+                },500, 'easeOutBack',function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+        });
+        }, function(){
+            $('#model-text4').html('').promise().done(function(){});
+            $('#model-text4').animate(
+                {
+                    fontSize:'0.5px',
+                },0,function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+            $("#model4").css({display:'inline-block'}).promise().done(function(){
+                $("#model4").animate({transform:'scale(1,1)'});
+        });
+    });
+
+    $("#model-text5").hover(function(){
+        $('#model-text5').html('Sample').promise().done(function(){});
+        $("#model5").css({display:'none'}).promise().done(function(){
+            $('#model-text5').animate(
+                {
+                    fontSize:'2px',
+                },500, 'easeOutBack',function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+        });
+        }, function(){
+            $('#model-text5').html('').promise().done(function(){});
+            $('#model-text5').animate(
+                {
+                    fontSize:'0.5px',
+                },0,function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+            $("#model5").css({display:'inline-block'}).promise().done(function(){
+                $("#model5").animate({transform:'scale(1,1)'});
+        });
+    });
+
+    $("#model-text6").hover(function(){
+        $('#model-text6').html('Sample').promise().done(function(){});
+        $("#model6").css({display:'none'}).promise().done(function(){
+            $('#model-text6').animate(
+                {
+                    fontSize:'2px',
+                },500, 'easeOutBack',function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+        });
+        }, function(){
+            $('#model-text6').html('').promise().done(function(){});
+            $('#model-text6').animate(
+                {
+                    fontSize:'0.5px',
+                },0,function(){
+                // alert('animate');
+                // width:10px;
+	            // height: 10px;
+            });
+            $("#model6").css({display:'inline-block'}).promise().done(function(){
+                $("#model6").animate({transform:'scale(1,1)'});
         });
     });
 });
